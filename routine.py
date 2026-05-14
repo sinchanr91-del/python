bd8c36f52b75d8e42478feca1f8c98f0e8794ccf
sinchanr91-del <sinchanr91@gmail.com>
2026-05-14 18:33:20 +0100

    Add files via upload

---
+++ routine.py
@@ -0,0 +1,13 @@
+print("My Daily Routine")
+# Step 1: Waking up
+print("I wake up at 7 AM.")
+
+# Step 2: Going to school
+print("I go to school at 8:30 AM.")
+
+# Step 3: Playing after school
+print("I play football in the evening.")
+
+# Step 4: Studying
+print("I study for 2 hours at night.")
+print("Total study hours in a week:", 2 * 7)
