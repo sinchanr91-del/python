2bfe3adc2229b5abdf4e4fef7e37d91c04fac079
sinchanr91-del <sinchanr91@gmail.com>
2026-05-14 19:20:20 +0100

    Add files via upload

---
+++ print.py
@@ -0,0 +1,4 @@
+print("hello world")
+print(5)
+print(10+20)
+print("10 + 20")
---
+++ title.py
@@ -0,0 +1,7 @@
+a=10
+b=20
+print(a)
+print(b)
+name = input ("enter your name")
+age = input("how old are are you")
+print("hello",name,"\nyour age is ",age,"\nwelcome to the world of programming")
