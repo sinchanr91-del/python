b156d077fd4f2fccb381a65aa4e69121a7ab1393
sinchanr91-del <sinchanr91@gmail.com>
2026-06-05 16:52:04 +0100

    Add files via upload

---
+++ summer_time .py
@@ -0,0 +1,18 @@
+weather=int(input("enter any degree to check"))
+if weather>21:
+    print("Rohan,you have to wear light clothes")
+else:
+    print("Rohan,you have to wear your jacket and pullover")
+
+
+
+
+
+
+
+
+
+
+
+
+
