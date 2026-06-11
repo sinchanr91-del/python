902da90aeb47270a78c56f84218d8c11a0c83e95
sinchanr91-del <sinchanr91@gmail.com>
2026-06-11 19:30:01 +0100

    Add files via upload

---
+++ and_or_operator.py
@@ -0,0 +1,17 @@
+a =12
+b = 10
+c =0
+if a and b and c:
+    print("all the numbers have boolean values as true")
+else:
+    print("at least one value has boolean value as false")
+
+x = 10
+y = -10
+c =0
+if a>0 or b>0:
+    print("either of the numbers is greater than 0")
+else:
+    print("no number is greater than 0")
+
+
