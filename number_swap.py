f4e3ee2ffe37d37222ccf705050ec91581122002
sinchanr91-del <sinchanr91@gmail.com>
2026-07-01 16:48:19 +0100

    Add files via upload

---
+++ number_swap.py
@@ -0,0 +1,5 @@
+number1 =int(input("enter a number"))
+number2 =int(input("enter a number"))
+number3 =int(input("enter a number"))
+
+print("Tiya,the  correct order of the numbers are",number2,number3,number1,"not",number1,number2,number3)
