4cef0f3a352e4f196f921929aa2c33c4aa3c9dbb
sinchanr91-del <sinchanr91@gmail.com>
2026-07-05 20:18:37 +0100

    Add files via upload

---
+++ reverse_string.py
@@ -0,0 +1,6 @@
+string =input("please enter any string")
+string2 =("")
+for i in string:
+    string2 = i+ string2
+print("\n original string =",string)
+print("\n Reversed String =",string2)
---
+++ sum_of_whole_numbers.py
@@ -0,0 +1,5 @@
+n =int(input("Enter the number, to find the sum of whole numbers "))
+sum = 0
+for i in range(1,n+ 1):
+    sum =sum +i
+    print ("sum =",sum)
