203128a1ee38d733f5d25b99e1999e5704d0e549
sinchanr91-del <sinchanr91@gmail.com>
2026-09-05 13:44:55 +0100

    Add files via upload

---
+++ floy_triangle.py
@@ -0,0 +1,8 @@
+row = int(input("please enter the number of rows"))
+num =1
+for i in range(1,row +1):
+    for j in range(1,i+ 1):
+        print(num,end =" ")
+        num = num +1
+    print( )
+    
